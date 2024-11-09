85adbe0725255eac194642371e10d90634483a26
Ryan Hotard <ryanhotard@Ryans-MBP.attlocal.net>
2024-11-09 03:14:01 -0600

    sequential query test

--- query-dispatcher.js
+++ query-dispatcher.js
@@ -2,10 +2,19 @@ import { nyaa_html_finder, nyaa_reserve_extract } from "./anime-finder-funcs.js"
 import pLimit from "p-limit";
 
 async function nyaa_function_dispatch(nyaa_queries, release_status_complete, fallback) {
-    const limit = pLimit(2);
+    /*const limit = pLimit(2);
     const nyaa_finder_promises = nyaa_queries.map(query => limit(() => nyaa_html_finder(...query)))
+    
+    const results = await Promise.all(nyaa_finder_promises); */
+    const results = [];
 
-    const results = await Promise.all(nyaa_finder_promises);
+    for (const query of nyaa_queries) {
+        const result = await nyaa_html_finder(...query);
+        results.push(result);
+
+        // Introduce a delay before the next function call
+        //await delay(500); // Delay of 500 milliseconds
+    }
     
     // Extract torrentList and reserve_cache from each result
     const torrents = results.map(result => result.torrentList);
