51cb9ebf9f16f1c3ff72f8492997c66fe88c8cde
Ryan Hotard <ryanhotard@Ryans-MBP.attlocal.net>
2024-11-09 02:53:57 -0600

    changing branch save

--- query-dispatcher.js
+++ query-dispatcher.js
@@ -1,26 +1,31 @@
 import { nyaa_html_finder, nyaa_reserve_extract } from "./anime-finder-funcs.js";
 import pLimit from "p-limit";
 
-async function nyaa_function_dispatch(nyaa_queries, release_status_complete) {
-    const limit = pLimit(5);
+async function nyaa_function_dispatch(nyaa_queries, release_status_complete, fallback) {
+    const limit = pLimit(2);
     const nyaa_finder_promises = nyaa_queries.map(query => limit(() => nyaa_html_finder(...query)))
 
     const results = await Promise.all(nyaa_finder_promises);
-    const torrents = results.torrentList; 
-    const reserve = results.reserve_cache;
+    
+    // Extract torrentList and reserve_cache from each result
+    const torrents = results.map(result => result.torrentList);
+    const reserve = results.map(result => result.reserve_cache);
+
 
     const reserve_torrents = reserve.flat();
     const uniq_reserve_torrentes = dedupeTorrents(reserve_torrents);
 
     const allTorrents = torrents.flat();
-    const unique_torrents = dedupeTorrents(allTorrents);
+    console.log('All Torrents:', allTorrents);
 
+    const unique_torrents = dedupeTorrents(allTorrents);
+    
     if (unique_torrents.length >= 1) {
         return unique_torrents;
     }
     else {
 
-        if (release_status_complete) {
+        if (release_status_complete && !fallback) {
             const episode = nyaa_queries[0][4];
             const checked_reserve_torrents = nyaa_reserve_extract(uniq_reserve_torrentes, episode);
             return checked_reserve_torrents;
@@ -31,6 +36,7 @@ async function nyaa_function_dispatch(nyaa_queries, release_status_complete) {
         
     }
 }
+
 
 function dedupeTorrents(torrents) {
     const uniqueTorrentsMap = new Map();
